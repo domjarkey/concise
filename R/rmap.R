1af9ece8f2a7df392c51e9414b27e61bc6201663
domjarkey <dominic.jarkey@gmail.com>
2023-10-19 16:06:06 +0100

    Add basic documentation for rmap

--- R/rmap.R
+++ R/rmap.R
@@ -1,3 +1,81 @@
+#' Map over rows of a data frame
+#'
+#' @description
+#' `rmap` iteratively applies an anonymous function to rows of a data frame or a
+#' named list of lists of equal length. `rmap` works similarly to `purrr::pmap`,
+#' except the data frame does not need to be subset to only those columns used by
+#' the function, and columns can be directly referred to by name inside the definition
+#' of the anonymous function.
+#'
+#' `rmap` also permits the usage of additional "pronouns" to concisely refer to
+#' related objects or properties of the data columns, such as row number, grouping
+#' (if the data frame is grouped using `dplyr::group_by` or similar), as well as
+#' the anonymous function itself for use in recursion.
+#'
+#' `rmap_chr`, `rmap_dbl`, `rmap_df`, `rmap_int`, and `rmap_lgl` work like their
+#' `purrr::pmap_etc` equivalents, attempting to output a vector of the specified
+#'  type (or a data.frame in the case of `rmap_df`) instead of a list.
+#'
+#' @param .l A data frame or named list of lists/vectors of equal length.
+#' @param .f A formula defining the anonymous function to be applied to every
+#' element of `.l`, in which `.x` refers to the given element. See Examples for
+#' how this differs from formulas in `purrr::map`.
+#' @param ... Additional named arguments are passed directly to the execution
+#' environment of the anonymous function as variables. These variables may include
+#' transformations on the entire `.l` object as well as any variables local to the
+#' calling environment of the `rmap` function.
+#' @param env Specify the parent environment of the execution environment of the
+#' anonymous function to be created. By default, this will be the environment in
+#' which `rmap` is called.
+#' @param map_fn The underlying function used to perform the mapping - this should
+#' not be chosen directly as safe options are prespecified by the defaults of the
+#' `_chr`, `_dbl`, `_df`, `_int`, and `_lgl` variants of `rmap`.
+#' @param simplify If `TRUE`, the function will attempt to flatten list outputs
+#' to a vector where possible (if "unlisting" is not possible, say, in the case of
+#' a list of lists of length > 1, the original output in list form will be returned).
+#'
+#' @details
+#' # Pronouns
+#' `rmap` supports reference to a set of useful "pronouns" that allow you to refer
+#' to other objects within the formula `.f` as though they were locally defined
+#' as variables. Note these are not pronouns in the `rlang` sense of the term, but
+#' a convenient shorthand to provide additional functionality in a readable format.
+#' Unlike its sister function `cmap`, `rmap` takes multiple named inputs, so row
+#' elements a referred to by the name of the column (here indicated as `<column_name>`)
+#' instead of `.x`.
+#' See Examples for further clarification on the usage of each. Supported pronouns
+#' are:
+#'
+#' * `<column_name>` -- in the formula, this refers to an individual element of
+#' the named data column.
+#' * `.i` -- the row index, or, if `.l` is grouped, the row index within the group.
+#' Equivalent to `dplyr::row_number()`.
+#' * `.I` -- the absolute row index; whether or not `.l` is grouped, this will
+#' return the overall position of the current row. Equivalent to
+#' `dplyr::cur_group_rows()`.
+#' * `<column_name>.nm` -- if the column `<column_name>` in `.l` is named, this
+#' returns the name corresponding to the current element of `<column_name>`; If
+#' this column of `.l` is unnamed, returns `NULL`. Note that columns in `tibble`
+#' data frames retain their `names` attribute, but ordinary base R data frames
+#' do not.
+#' * `.n` -- the index of the final row in `.l`, or, if `.l` is grouped, the final
+#' row in the given group. Equivalent to `dplyr::n()`.
+#' * `.N` -- the index of the final row in `.l`, whether or not `.l` is grouped.
+#' Equivalent to `nrow(.l)`.
+#' * `<column_name>.grp` -- all elements in the current group as a list object.
+#' If `.l` is not grouped, returns the same as `<column_name>.col`, i.e. the entire
+#' column `<column_name>` as a list object.
+#' * `<column_name>.col` -- the entire data column `<column_name>` in `.l`, as
+#' opposed to just the current element. Returns the full column whether or not `.l`
+#' is grouped.
+#' * `.this` -- the anonymous function itself, to be used in cases where recursion
+#' is needed.
+#' @returns Returns a list (or vector) of the same length as `.l`. By default a
+#' list, unless `rmap_` suffixes are used to specify the output vector type, or
+#' if `simplify=TRUE` a vector of whichever type uniformly fits the unlisted
+#' outputs.
+#'
+#'
 #' @import rlang
 #' @export
 rmap <- function(
@@ -11,6 +89,10 @@ rmap <- function(
     # TODO: type checking of arguments with errors
     # TODO: implement .x for length(.l) == 1 / .l atomic
     # TODO: implement .x for name in rhs of .f
+    # TODO: add .row pronoun as a 1-row tibble with all columns
+    # TODO: add pronoun to check group membership maybe? Does this make sense?
+    # TODO: change .l to .data maybe?
+    # TODO: remove map_fn & simplify args from suffix functions
 
     .f <- get_rhs(rlang::enexpr(.f))
 
