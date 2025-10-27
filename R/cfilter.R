d0a8e29713c88f2c9bce2110acfda27abb7e71a7
domjarkey <dominic.jarkey@gmail.com>
2025-10-27 21:18:22 +0000

    Implement cfilter function and include new ensure_concise_type utility function that always casts concise expressions with a ? lgl modifier inside cfilter

---
+++ R/cfilter.R
@@ -0,0 +1,46 @@
+#' Filter rows with Concise notation
+#'
+#' @description
+#' `cfilter` behaves identically to [dplyr::filter] while adding support for
+#' Concise notation and pronouns, mirroring the behaviour of [cmutate].
+#' Expressions that begin with `~` are interpreted as concise formulas and are
+#' evaluated rowwise using the same pronouns that `cmutate` recognises. These
+#' expressions can also specify optional mappings and additional arguments using
+#' the `?` operator, just like `cmutate`.
+#'
+#' @param .data A data frame.
+#' @param ... Expressions passed on to [dplyr::filter]. Any expression that
+#'   begins with `~` is treated as a concise formula and gains access to the
+#'   additional pronouns and helpers provided by [cmutate].
+#'
+#' @return A data frame containing the rows that satisfy all filtering
+#'   expressions.
+#'
+#' @examples
+#' tibble::tibble(x = list("apple", NULL, "banana")) |>
+#'   cfilter(~ !is.null(x))
+#'
+#' tibble::tibble(x = 1:5) |>
+#'   cfilter(~ .i %% 2 == 1)
+#'
+#' @export
+cfilter <- function(.data, ...) {
+    .args <- rlang::enquos(...)
+    .out <- .data
+
+    for (i in seq_along(.args)) {
+        .expr <- rlang::quo_get_expr(.args[[i]])
+
+        if (is_concise_formula(.expr)) {
+            .expr <- ensure_concise_type(.expr, "lgl")
+            .args[[i]] <- rlang::quo_set_expr(
+                .args[[i]],
+                parse_concise_expression(.out, !!.expr)
+            )
+        }
+
+        .out <- dplyr::filter(.out, !!.args[[i]])
+    }
+
+    .out
+}
